621a8aec9f53f780746a4caf1e9f7b48c49e6c32
Andreas Alexopoulos <andalexo@gmail.com>
2021-01-14 00:56:13 +0200

    adds docstring to top module level, exports set_logging to top level

--- yaplog/__init__.py
+++ yaplog/__init__.py
@@ -1,3 +1,14 @@
+"""yaplog
+
+Yet Another Python Logging module.
+Helper module to set up a logging service with some predefined defaults.
+For usage:
+>>> from yaplog import set_logging
+>>> help(set_logging)
+"""
+
 __version__ = '1.0'
 __version_info__ = (1, 0, 0)
 __name__ = 'yaplog'
+
+from .logging import set_logging
